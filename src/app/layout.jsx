1db6cde0bf9e9121d601e1f47c601ae16926a15d
shubham4519 <shubhamsinha012345@gmail.com>
2025-07-11 16:13:55 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -48,7 +48,7 @@ export default function RootLayout({ children }) {
           <img
             height="1"
             width="1"
-            style={{ display: "none" }}
+            style={{ display: 'none' }}
             src="https://www.facebook.com/tr?id=3547056045603774&ev=PageView&noscript=1"
             alt="facebook pixel"
           />
