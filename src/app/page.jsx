55fb877611633780d9dc5da2a8ed93ebebb9dc7a
shubham4519 <shubhamsinha012345@gmail.com>
2025-09-08 19:34:32 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -27,7 +27,7 @@ export default function Page() {
 
                     <div className='items-center w-full md:w-[25%]'>
                         <Mail className='size-6 mr-2' />
-                        <div className='text-[14px] break-words'>hello@meeshoregistration-valmo.in 
+                        <div className='text-[14px] break-words'>hello@valmo.in 
                         </div>
                     </div>
 
@@ -38,7 +38,7 @@ export default function Page() {
                     </div>
                 </div>
                 <p className='px-8 md:px-28 py-4 text-[15px] leading-4 font-mono'>
-                    Disclaimer: Any official communication for business related formalities will be sent by Valmo using our authorised official email addresses (@meeshoregistration-valmo.in 
+                    Disclaimer: Any official communication for business related formalities will be sent by Valmo using our authorised official email addresses (@valmo.in 
                     or @meesho.com). Kindly DO NOT interact with any communications or requests for payments from any other sources or share any personal information.
                 </p>
                 <div className='px-8 md:px-28 pt-2 text-gray-200 border-t-[1px] border-gray-400'>
