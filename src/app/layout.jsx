2c628b06f75e8a37e7c70839b0c4d0274ff14ea0
shubham4519 <shubhamsinha012345@gmail.com>
2025-11-13 20:39:22 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,34 +22,30 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-         <Script
-          id="facebook-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1443910160081414');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1378021340619910');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
 
         {/* Meta Pixel NoScript */}
         <noscript>
-         <img
+          <img
             height="1"
             width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1443910160081414&ev=PageView&noscript=1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1378021340619910&ev=PageView&noscript=1"
           />
         </noscript>
 
